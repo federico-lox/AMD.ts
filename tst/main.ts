5864c7a5c1bedb8184b0cf61b7200fe1d2cbd201
Federico "Lox" Lucignano <federico-lox@outlook.com>
2016-05-26 21:53:48 +0200

    Wrap implementation in a namespace to avoid pollution

--- tst/main.ts
+++ tst/main.ts
@@ -1,5 +1,8 @@
 /// <reference path="../src/amd.ts" />
-declare var require: Function;
+declare const
+    global: any,
+    require: any;
+    
 const assert = require('assert');
 
 global.require(['tst/fizz'], (fizz: any) => assert.strictEqual(fizz.buzz(), 5));
