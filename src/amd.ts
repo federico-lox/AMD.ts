d8d91a68258b652e2f9f941771464f3b3af6516e
Federico "Lox" Lucignano <federico-lox@outlook.com>
2016-05-26 14:24:27 +0200

    Abstract common types

--- src/amd.ts
+++ src/amd.ts
@@ -1,12 +1,15 @@
 declare var exports;
 
 ((context) => {
+    type defmodule = (...deps: any[]) => any;
+    type deplist = string[];
+
     const tracker = {};
     const definitions = {};
 
-    function require() { }
+    function require(dependencies: deplist, definition: defmodule) { }
 
-    function define(name: string, dependencies: string[], definition: (...deps: any[]) => any) {
+    function define(name: string, dependencies: deplist, definition: defmodule) {
         if (isVoid(name) || isVoid(dependencies) || isVoid(define)) {
             throw new Error(`Missing or null parameters: name ${name} - dependencies ${dependencies} - definition ${definition}`);
         }
