a0b61fcf9bd6df2de1491162b514dba8afd9803d
Federico "Lox" Lucignano <federico-lox@outlook.com>
2016-05-26 21:59:26 +0200

    Add tests covering mixed sequence of define/require

--- tst/main.ts
+++ tst/main.ts
@@ -5,4 +5,14 @@ declare const
     
 const assert = require('assert');
 
+// Test variants of modules emitted by tsc
 global.require(['tst/fizz'], (fizz: any) => assert.strictEqual(fizz.buzz(), 5));
+
+// Test mixed sequence of inter-dependent modules
+global.define('a', [], () => 2)
+global.require(['b', 'c', 'a'], (b: any, c: any, a: any) => assert.strictEqual(b + c.a + a, 10));
+global.define('b', ['c'], (c: any) => c.a + 2);
+global.define('c', ['a'], (a: any) => ({ a: 1 + a }));
+global.require(['a', 'd'], (a: any, d: any) => assert.strictEqual(a + d.z, 10));
+global.define('d', ['e'], (e: any) => ({ z: 4 + e }));
+global.define('e', ['a'], (a: any) => 2 + a);
