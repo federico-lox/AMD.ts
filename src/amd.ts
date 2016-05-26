54fe7fe89f75e225f51509cc9f9c251ea41f9b64
Federico "Lox" Lucignano <federico-lox@outlook.com>
2016-05-26 18:56:58 +0200

    Add require and refactor to support lazy definitions

--- src/amd.ts
+++ src/amd.ts
@@ -1,21 +1,30 @@
-declare var exports;
+// TODO: Support reloading/re-defining modules
+declare var exports, global;
 
 ((context) => {
     type defmodule = (...deps: any[]) => any;
-    type deplist = string[];
+    type depslist = string[];
 
-    const tracker = {};
-    const definitions = {};
+    const
+        inverseDependencyMap = {},
+        modules = {},
+        initializers = {};
 
-    function require(dependencies: deplist, definition: defmodule) { }
-
-    function define(name: string, dependencies: deplist, definition: defmodule) {
-        if (isVoid(name) || isVoid(dependencies) || isVoid(define)) {
-            throw new Error(`Missing or null parameters: name ${name} - dependencies ${dependencies} - definition ${definition}`);
+    function require(dependencies: depslist, definition: defmodule) {
+        if (isVoid(dependencies) || isVoid(definition)) {
+            throw new Error(`Missing or null parameters: dependencies ${dependencies} - definition ${definition}`);
+        } else {
+            processModule(`require-${Date.now()}.${Math.random()}`, dependencies, definition);
         }
+    }
 
-        dependencies.forEach((dep) => dep in tracker ? tracker[dep].push(name) : tracker[dep] = [name]);
-        definitions[name] = { dependencies, definition };
+    function define(name: string, dependencies: depslist, definition: defmodule) {
+        if (isVoid(name) || isVoid(dependencies) || isVoid(definition)) {
+            throw new Error(`Missing or null parameters: name ${name} - dependencies ${dependencies} - definition ${definition}`);
+        } else {
+            // TODO: remove from modules if exists to allow reloading/redefining in the future
+            processModule(name, dependencies, definition);
+        }
     }
 
     context.require = require;
@@ -25,4 +34,39 @@ declare var exports;
         // true for both undefined and null
         return value == null;
     }
-})(typeof exports !== 'undefined' ? exports : window);
+
+    function processModule(name: string, dependencies: depslist, definition: defmodule) {
+        if (dependencies.length === 0) {
+            modules[name] = definition();
+        } else {
+            if (!(name in initializers)) {
+                initializers[name] = initializer.bind(null, name, dependencies, definition);
+            }
+
+            dependencies.forEach((dep) => {
+                if (!(dep in inverseDependencyMap)) inverseDependencyMap[dep] = {};
+                inverseDependencyMap[dep][name] = null;          
+                processDependencies(dep);
+            });
+        }
+    }
+
+    function initializer(id: string, dependencies: depslist, definition: defmodule) {
+        // Counting down from deps.length to 0 using a local counter could be an optimization although less safe
+        const missingCount = dependencies.filter((dep) => !(dep in modules)).length;
+
+        if (missingCount === 0) {
+            modules[id] = definition.apply(null, dependencies.map((dep) => modules[dep]));
+            processDependencies(id);
+            initializers[id] = null;
+        }
+    }
+
+    function processDependencies(name: string) {
+        if (name in inverseDependencyMap) {
+            Object.keys(inverseDependencyMap[name]).forEach((parent) => {
+                if (!isVoid(initializers[parent])) initializers[parent]();
+            });
+        }
+    }
+})(typeof exports !== 'undefined' ? exports : typeof window !== 'undefined' ? window : global);
